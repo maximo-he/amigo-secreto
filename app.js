559b6dfaaa66be305627abbccd71c4ba80529579
Máximo Hernández Higuera <maxhernan.av@gmail.com>
2025-06-28 23:36:25 -0400

    Actualiza app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //Declaración de variable de tipo array para los nombres de los amigos.
-let amigos = ["Juan", "María", "Pedro", "Diego", "Sofía"];
+let amigos = ["Juan", "María", "Pedro", "Diego", "Sofía", "Sonia"];
 
 // Obtén el elemento del campo de texto
 let campoTexto = document.getElementById("nombreAmigo");
