4bf8a8a140d3858be41c32d66370fc600548df02
Máximo Hernández Higuera <maxhernan.av@gmail.com>
2025-08-01 01:08:20 -0400

    Actualización de lista

--- app.js
+++ app.js
@@ -21,3 +21,19 @@ function agregarAmigo() {
     mostrarListaAmigos(),
 
 }
+
+function mostrarListaAmigos() {
+    //llamar la lista con su id
+    let lista = document.getElementById("listaAmigos");
+
+    //clear lista existente (evita duplicación)
+    lista.innerHTML = ""
+
+    //iteración con el arreglo y creación de lista
+    for (let amigo of amigos) {
+        let li = document.createElement('li');
+        li.textContent = amigo;
+        lista.appendChild(li);
+    }
+}
+}
