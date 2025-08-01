34b07e09c3f3e69941ccfca9d051d70421a8d8fc
Máximo Hernández Higuera <maxhernan.av@gmail.com>
2025-08-01 01:05:15 -0400

    Creación array e función para agregar amigos

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
-//Declaración de variable de tipo array para los nombres de los amigos.
 let amigos = []
 let indiceAleatorio = 0;
 let amigoSorteado = 0;
 
 function agregarAmigo() {
-  //captura de nombre y clear texto inicial (trimp)
-  let capturaNombreAmigo = document.
+    //captura de nombre y clear texto inicial (trimp)
+    let capturaNombreAmigo = document.getElementById('amigo').value.trim();
+  
+    //validación entrada
+    if (capturaNombreAmigo === "") {
+        alert('Por favor, ingresa un nombre');
+        return;
+    }
+    //añadir nombre al array
+    amigos.push(capturaNombreAmigo);
+  
+    //clear campo
+    document.getElementById('amigo').value = "";
+  
+    //mostrar lista agregada
+    mostrarListaAmigos(),
+
 }
-let campoTexto = document.getElementById("nombreAmigo");
